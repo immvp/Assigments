3e8bacf910749472f0d75ce239a281d1baa9de6d
Mvq <mvpberger@gmail.com>
2023-09-19 20:09:42 +0200

    test done

--- A0/file.c
+++ A0/file.c
@@ -26,10 +26,13 @@ int PrintSuccess(char str[], FileTypes fileTypes) {
         strcpy(fileType, "empty");
         break;
     case ASCII:
-        strcpy(fileType, "ascii");
+        strcpy(fileType, "ASCII text");
         break;
     case UTF:
-        strcpy(fileType, "utf");
+        strcpy(fileType, "UTF-8 text");
+        break;
+    case ISO:
+        strcpy(fileType, "ISO-8859 text");
         break;
     default:
         strcpy(fileType, "data");
@@ -44,15 +47,8 @@ enum FileTypes CheckType(FILE *file) {
     int size;
     unsigned char* buffer;
 
-    // Each index corresponds to the byte number for utf-8
-    int utfCounter[3];
-
-    for (size_t i = 0; i < 3; i++)
-    {
-        utfCounter[i] = 0;
-    }
+    unsigned int* utfCounter = (unsigned int*) calloc(3, sizeof(unsigned int));
     
-
     // Counter for searching for bytes
     int byteMissing = 0;
     // Byte Number we are searching for
@@ -71,28 +67,52 @@ enum FileTypes CheckType(FILE *file) {
     // Sliding Window Pattern
     // Check om de første 3 bits er 110
     for (int i = 0; i < size; i++) {
-        // If no continous byte before this one, and its between 10000000 and 10111111
+
+        // If NULL then data
+        if (buffer[i] == 0x0) {
+            return data;
+        }
+
+        // Unused bytes in ISO
+        if (0x80 <= buffer[i] && buffer[i] <= 0x9F) {
+            return UTF;
+        }
+
+        // If no header byte before this one, and its between 10000000 and 10111111
         if (byteMissing == 0 && (0x80 <= buffer[i] && buffer[i] <= 0xBF)) {
             return ISO;
         }
+        
         // 110xxxxx
         if (0xC0 <= buffer[i] && buffer[i] <= 0xDF) {
+            // If we are searching, then it has to be ISO
+            if (byteMissing != 0) {
+                return ISO;
+            }
             numByte = 0;
             byteMissing = 1;
         }
         // 1110xxxx
         if (0xE0 <= buffer[i] && buffer[i] <= 0xEF) {
+            // If we are searching, then it has to be ISO
+            if (byteMissing != 0) {
+                return ISO;
+            }            
             numByte = 1;
             byteMissing = 2;
 
         }
         // 11110xxx
         if (0xF0 <= buffer[i] && buffer[i] <= 0xF7) {
+            // If we are searching, then it has to be ISO
+            if (byteMissing != 0) {
+                return ISO;
+            }            
             numByte = 2;
             byteMissing = 3;
         }
         // 10xxxxxx
-        if (byteMissing != 0 && (0x80 <= buffer[i] && buffer[i] <= 0xBF)) {
+        if (byteMissing != 0 && (0xA0 <= buffer[i] && buffer[i] <= 0xBF)) {
             byteMissing--;
             if (byteMissing == 0) {
                 utfCounter[numByte]++;
@@ -102,16 +122,16 @@ enum FileTypes CheckType(FILE *file) {
     } 
 
     // Hacky solution for "end of file"-case. 
-    // If the last byte of our file is a continous byte then it has to be ISO.
+    // If the last byte of our file is a header byte then it has to be ISO.
     if (byteMissing != 0) {
         return ISO;
     }
 
-    if (utfCounter[1] > 0 || utfCounter[2] > 0) {
+    if (utfCounter[0] > 0 || utfCounter[1] > 0 || utfCounter[2] > 0) {
         return UTF;
     }
 
-    return data;
+    return ASCII;
 }
 
 
@@ -127,12 +147,7 @@ int main(int argc, char* argv[]) {
 
     // Determine if the file exists
     if ((file = fopen(argv[1], "rb")) != NULL) {
-        
-        CheckType(file);
-        PrintSuccess(argv[1], empty);
-        exit(EXIT_SUCCESS);
-        // Determine if it's empty
-        
+        PrintSuccess(argv[1], CheckType(file));
 
         // We close file when done using it
         fclose(file);
---
+++ A0/test.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# Exit immediately if any command below fails.
+set -e
+
+make
+
+
+echo "Generating a test_files directory.."
+mkdir -p test_files
+rm -f test_files/*
+
+
+echo "Generating test files.."
+printf "Hello, World!\n" > test_files/ascii.input
+printf "Hello, World!" > test_files/ascii2.input
+printf "Hello,\x00World!\n" > test_files/data.input
+printf "" > test_files/empty.input
+### TODO: Generate more test files ###
+# uses 2 header bytes after each other
+printf "\xf0\xf0" > test_files/iso_1.input
+# could be utf, could be iso (same pattern) - we go with utf
+printf "\xd7\xa2" > test_files/iso_2.input
+printf "\x20\x20\xd7\xa2\x20\x20" > test_files/iso_3.input
+# double continous, must be ascii
+printf "\x6f\x6f" > test_files/ascii3.input
+#only contains ascii .. must be ascii
+printf "abc" > test_files/ascii4.input
+# ȁ - uses unused bytes, between 128-159 decimal values
+printf "\x20\x20\xc4\x9f\x20\x20" > test_files/utf_1.input
+#Contains ANY utf pattern. We assume its utf.
+printf "\x20\x20\xc6\xa3\x20\x20" > test_files/utf_2.input
+
+
+
+echo "Running the tests.."
+exitcode=0
+for f in test_files/*.input
+do
+  echo ">>> Testing ${f}.."
+  file    ${f} | sed -e 's/ASCII text.*/ASCII text/' \
+                         -e 's/UTF-8 Unicode text.*/UTF-8 Unicode text/' \
+                         -e 's/ISO-8859 text.*/ISO-8859 text/' \
+                         -e 's/writable, regular file, no read permission/cannot determine (Permission denied)/' \
+                         > "${f}.expected"
+  ./file  "${f}" > "${f}.actual"
+
+  if ! diff -u "${f}.expected" "${f}.actual"
+  then
+    echo ">>> Failed :-("
+    exitcode=1
+  else
+    echo ">>> Success :-)"
+  fi
+done
+exit $exitcode
