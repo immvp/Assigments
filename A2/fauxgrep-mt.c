c54c21170eef4ad035fdaa5b1bd65d36ce8b6c28
immvp <mvpberger@gmail.com>
2023-10-28 12:21:40 +0200

    test push

--- A2/fauxgrep-mt.c
+++ A2/fauxgrep-mt.c
@@ -148,7 +148,17 @@ int main(int argc, char * const *argv) {
 
   fts_close(ftsp);
 
-  assert(0); // Shut down the job queue and the worker threads here.
+  // Destroy the queue.
+  job_queue_destroy(&jq);
+
+  // Wait for all threads to finish.  This is important, at some may
+  // still be working on their job.
+  for (int i = 0; i < num_threads; i++) {
+    if (pthread_join(threads[i], NULL) != 0) {
+      err(1, "pthread_join() failed");
+    }
+  }
+  free(threads);
 
   return 0;
 }
