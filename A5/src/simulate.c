3fa436d2aae6b3bd8465998731e28f2a4e62c922
Itzchaky <itzchaky@hotmail.com>
2023-12-11 13:47:34 +0100

    A5

---
+++ A5/src/simulate.c
@@ -0,0 +1,12 @@
+#include "read_exec.h"
+#include "assembly.h"
+#include "simulate.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE *log_file) {
+    printf("HJEJ");
+    return (2);
+} 
